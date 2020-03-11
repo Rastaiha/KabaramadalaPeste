089cb8b3239f4ceee0cada99c5e4b2b637fa72b4
ab.reza <a.re.morteza@gmail.com>
2020-03-11 15:11:44 +0330

    init front

--- ra/urls.py
+++ ra/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test_base/', lambda r: render(r, 'base/base.html'))
 ]
