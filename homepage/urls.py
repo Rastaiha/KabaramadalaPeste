23e4ef32b071c64421d594df34fe9ee43e249125
ab.reza <a.re.morteza@gmail.com>
2020-03-14 16:49:32 +0330

    Add our team and about us page. Add gender selection to sign up

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.urls import path
 
-from .views import homepage
+from .views import homepage, our_team, about_us
 
 urlpatterns = [
     path('', homepage, name="homepage"),
     path('test_auth_base/', lambda r: render(r, 'auth/signup.html', {
         "nol": True
     })),
+    path('our_team/', our_team),
+    path('about_us/', about_us),
 ]
