3917696215e4fa945c369e8c8eb0e6a439e345ed
Erfan Loghmani <Elkiscoming@gmail.com>
2020-03-19 22:55:03 +0330

    Add island, challenge, question models & admin

--- kabaramadalapeste/admin.py
+++ kabaramadalapeste/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
 # Register your models here.
+from kabaramadalapeste.models import (
+    Island, Challenge, ChallengeRewardItem, ShortAnswerQuestion, JudgeableQuestion
+)
+from django.forms.models import BaseInlineFormSet
+
+
+class RewardInlineFormSet(BaseInlineFormSet):
+    def __init__(self, *args, **kwargs):
+        kwargs['initial'] = list(map(lambda x: {'reward_type': x},
+                                     ChallengeRewardItem.REWARD_TYPE_CHOICES))
+        super(RewardInlineFormSet, self).__init__(*args, **kwargs)
+
+
+class ChallengeRewardInline(admin.TabularInline):
+    model = ChallengeRewardItem
+    min_num = len(ChallengeRewardItem.REWARD_TYPE_CHOICES)
+    extra = 0
+    formset = RewardInlineFormSet
+
+
+@admin.register(Challenge)
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [
+        ChallengeRewardInline,
+    ]
+
+
+@admin.register(ShortAnswerQuestion)
+class ShortAnswerQuestionAdmin(admin.ModelAdmin):
+    def render_change_form(self, request, context, *args, **kwargs):
+        context['adminform'].form.fields['challenge'].queryset = Challenge.objects.filter(is_judgeable=False)
+        return super(ShortAnswerQuestionAdmin, self).render_change_form(request, context, *args, **kwargs)
+
+
+@admin.register(JudgeableQuestion)
+class JudgeableQuestionAdmin(admin.ModelAdmin):
+    def render_change_form(self, request, context, *args, **kwargs):
+        context['adminform'].form.fields['challenge'].queryset = Challenge.objects.filter(is_judgeable=True)
+        return super(JudgeableQuestionAdmin, self).render_change_form(request, context, *args, **kwargs)
+
+
+admin.site.register(Island)
--- kabaramadalapeste/models.py
+++ kabaramadalapeste/models.py
@@ -1,3 +1,91 @@
 from django.db import models
 
-# Create your models here.
+
+class Island(models.Model):
+    island_id = models.PositiveIntegerField(unique=True)
+    name = models.CharField(max_length=50)
+    challenge = models.ForeignKey('Challenge',
+                                  on_delete=models.SET_NULL,
+                                  blank=True,
+                                  null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=50)
+    is_judgeable = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class ChallengeRewardItem(models.Model):
+    SEKKE = 'SK'
+    KEY1 = 'K1'
+    KEY2 = 'K2'
+    KEY3 = 'K3'
+    REWARD_TYPE_CHOICES = [
+        (SEKKE, 'sekke'),
+        (KEY1, 'key 1'),
+        (KEY2, 'key 2'),
+        (KEY3, 'key 3'),
+    ]
+    reward_type = models.CharField(
+        max_length=2,
+        choices=REWARD_TYPE_CHOICES,
+        default=SEKKE,
+    )
+    amount = models.IntegerField(default=0)
+    challenge = models.ForeignKey('Challenge',
+                                  related_name='rewards',
+                                  on_delete=models.SET_NULL,
+                                  blank=True,
+                                  null=True)
+
+    def __str__(self):
+        return '%s %s %s' % (
+            self.challenge.name, self.reward_type, self.amount
+        )
+
+    class Meta:
+        unique_together = (("challenge", "reward_type"),)
+
+
+class BaseQuestion(models.Model):
+    title = models.CharField(max_length=100)
+    question = models.FileField(upload_to='soals/')
+
+    challenge = models.ForeignKey('Challenge',
+                                  related_name='%(class)s',
+                                  on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return '%s for challenge: %s' % (
+            self.title, self.challenge.name
+        )
+
+
+class ShortAnswerQuestion(BaseQuestion):
+    INTEGER = 'INT'
+    FLOAT = 'FLT'
+    STRING = 'STR'
+    ANSWER_TYPE_CHOICES = [
+        (INTEGER, 'int'),
+        (FLOAT, 'float'),
+        (STRING, 'string')
+    ]
+    correct_answer = models.CharField(max_length=50)
+    answer_type = models.CharField(
+        max_length=3,
+        choices=ANSWER_TYPE_CHOICES,
+        default=STRING,
+    )
+
+
+class JudgeableQuestion(BaseQuestion):
+    pass
