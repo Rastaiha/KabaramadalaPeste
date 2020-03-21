3a1737d6e18ab67f6583e7b078a3e36f30c4faec
artajmir3 <artajmir3@yahoo.com>
2020-03-21 18:54:45 +0430

    minor change

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class Member(AbstractUser):
+    is_participant = models.BooleanField(default=True)
+
+
+class Participant(models.Model):
+    member = models.ForeignKey(Member, related_name='participant', on_delete=models.CASCADE)
+    school = models.CharField(max_length=50)
+    city = models.CharField(max_length=20)
+    document = models.ImageField(upload_to='documents/')
+
+
+class Judge(models.Model):
+    member = models.ForeignKey(Member, related_name='judge', on_delete=models.CASCADE)
