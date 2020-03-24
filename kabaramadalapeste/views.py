798658ecb67a036e933432f64137f201150bfa7a
ab.reza <a.re.morteza@gmail.com>
2020-03-24 20:38:39 +0430

    fix issue

--- kabaramadalapeste/views.py
+++ kabaramadalapeste/views.py
@@ -3,8 +3,6 @@ from django.http import Http404
 
 
 def game(request):
-    raise Http404
-    '''
     return render(request, 'kabaramadalapeste/game.html', {
         'without_nav': True,
         'without_footer': True,
