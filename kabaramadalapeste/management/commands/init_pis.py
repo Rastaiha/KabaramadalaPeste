6f8f924a6a3aad5ae04d7dc3a12c043e968bfcbc
artajmir3 <artajmir3@yahoo.com>
2020-03-27 20:42:48 +0430

    atomic

--- kabaramadalapeste/management/commands/init_pis.py
+++ kabaramadalapeste/management/commands/init_pis.py
@@ -1,10 +1,12 @@
 from django.core.management.base import BaseCommand
 from accounts.models import Participant
+from django.db import transaction
 
 
 class Command(BaseCommand):
     help = 'Displays current time'
 
+    @transaction.atomic
     def handle(self, *args, **kwargs):
         for participant in Participant.objects.all():
             participant.init_pis()
