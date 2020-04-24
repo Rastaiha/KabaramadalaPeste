448e390cdde5f9d8d778c1948d3f3c918d8929e6
Erfan Loghmani <Elkiscoming@gmail.com>
2020-04-24 18:06:54 +0430

    Create initial members

---
+++ accounts/management/commands/create_participants.py
@@ -0,0 +1,46 @@
+import csv
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from accounts.models import Member, Participant
+from django.conf import settings
+import os
+import logging
+
+logger = logging.getLogger(__file__)
+
+
+class Command(BaseCommand):
+    help = 'Create participants from csv'
+
+    def __init__(self, *args, **kwargs):
+        self.base_dir = os.path.join(settings.BASE_DIR, 'accounts/initial_data')
+        self.participants_file = os.path.join(self.base_dir, 'participants.csv')
+        super(Command, self).__init__(*args, **kwargs)
+
+    def add_arguments(self, parser):
+        pass
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        with open(self.participants_file, newline='') as csvfile:
+            start_count = Participant.objects.count()
+            self.stdout.write('Participant currently has %s records' % start_count)
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for i, row in enumerate(reader):
+                if not i:
+                    self.stdout.write('Headers: ' + ', '.join(row))
+                else:
+                    member = Member.objects.create(
+                        username=row[0],
+                        email=row[1],
+                    )
+                    member.set_password(row[2])
+                    participant = Participant.objects.create(
+                        member=member,
+                        document_status='Verified',
+                        is_activated=True
+                    )
+                    member.save()
+                    participant.save()
+            self.stdout.write('Created %s new Participants' % (Participant.objects.count() - start_count))
+            self.stdout.write('Participant currently has %s records' % Participant.objects.count())
