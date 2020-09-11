bb4daca5d46bd3c996c15b9017c51ce2e98ef3d8
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2020-09-11 20:12:30 +0430

    update queries for get_number_of_challenge_of_each_team

--- accounts/queries.py
+++ accounts/queries.py
@@ -4,7 +4,7 @@ from django.db.models import Q, Count, Sum
 
 def get_number_of_teams_traveling():
     return Team.objects.all().values("id", "group_name").annotate(
-        sum_move=Count('participant__logs__id', filter=Q(participant__logs__event_type="Move"))).order_by('-sum_move')
+        sum_move=Count('participant__logs__id', filter=Q(participant__logs__event_type="Move"), distinct=True)).order_by('-sum_move')
 
 
 def get_number_of_coins_of_each_team():
@@ -15,12 +15,22 @@ def get_number_of_coins_of_each_team():
 
 def get_number_of_bully_of_each_team():
     return Team.objects.all().values("id", "group_name").annotate(
-        sum_BLY=Count('participant__logs__id', filter=Q(participant__logs__event_type="Bully target"))
+        sum_BLY=Count('participant__logs__id', filter=Q(participant__logs__event_type="Bully target"), distinct=True)
     ).order_by('-sum_BLY')
 
 
 def get_number_of_kheftgiri_of_each_team():
     return Team.objects.all().values("id", "group_name").annotate(
         count_BLY=Count('participant__member__notifications__id',
-                        filter=Q(participant__member__notifications__verb="sb_fall_in_your_bully"))
+                        filter=Q(participant__member__notifications__verb="sb_fall_in_your_bully"), distinct=True)
     ).order_by('-count_BLY')
+
+
+def get_number_of_challenge_of_each_team():
+    return Team.objects.all().values("id", "group_name").annotate(
+        count_challenge_accept=Count('participant__member__notifications__id',
+                                     filter=Q(
+                                         participant__member__notifications__verb="correct_judged_answer"
+                                     ) | Q(
+                                         participant__member__notifications__verb="correct_short_answer"),
+                                     distinct=True)).order_by('-count_challenge_accept')
