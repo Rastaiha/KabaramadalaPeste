3de227982c69ffa1266b28e93e616fe7963e6bdf
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2022-03-29 17:38:33 +0430

    enable username & email

--- accounts/admin.py
+++ accounts/admin.py
@@ -107,7 +107,7 @@ class MemberAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ('username', 'real_name', 'get_city', 'get_school', 'is_active', 'get_is_paid', 'get_document_status',
                     'get_document', 'account_actions', 'get_has_seen_day1', 'get_has_seen_day2')
     list_filter = ('is_active', IsPaidFilter, IsVerifiedFilter)
-    readonly_fields = ['username', 'email']
+    # readonly_fields = ['username', 'email']
     fields = ['first_name', 'username', 'email', 'is_active', 'is_participant']
     inlines = [ParticipantInline]
 
