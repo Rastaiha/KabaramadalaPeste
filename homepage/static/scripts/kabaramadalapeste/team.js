dba97970dbc01895e2c95cafcd0eec3daee49c02
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-08-17 13:56:49 +0330

    fix chatFrame

--- homepage/static/scripts/kabaramadalapeste/team.js
+++ homepage/static/scripts/kabaramadalapeste/team.js
@@ -1,9 +1,9 @@
 var jitsi = document.getElementById('jitsi');
 
 const chatFrame = document.createElement("iframe");
-chatFrame.src = jitsi.chat_room_link;
-chatFrame.width = "400";
-chatFrame.height = "100";
+chatFrame.src = jitsi.chatRoomLink;
+chatFrame.width = "100%";
+chatFrame.height = "300px";
 chatFrame.style.border = "none";
 chatFrame.style.background = "white";
 
